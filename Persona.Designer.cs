532b16b5e0d0332237b55f81d922e7ff70cf8710
daniel3scripts <paul.lugo@tecsup.edu.pe>
2022-09-06 14:54:19 -0500

    comit01

---
+++ Persona.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Lab03
+{
+    partial class Persona
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnListar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dvgListado = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dvgListado)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnListar
+            // 
+            this.btnListar.Location = new System.Drawing.Point(25, 33);
+            this.btnListar.Name = "btnListar";
+            this.btnListar.Size = new System.Drawing.Size(80, 25);
+            this.btnListar.TabIndex = 0;
+            this.btnListar.Text = "Listar";
+            this.btnListar.UseVisualStyleBackColor = true;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(357, 33);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(80, 25);
+            this.btnBuscar.TabIndex = 1;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(195, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(156, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // dvgListado
+            // 
+            this.dvgListado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dvgListado.Location = new System.Drawing.Point(34, 99);
+            this.dvgListado.Name = "dvgListado";
+            this.dvgListado.Size = new System.Drawing.Size(398, 297);
+            this.dvgListado.TabIndex = 3;
+            // 
+            // Persona
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 431);
+            this.Controls.Add(this.dvgListado);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnListar);
+            this.Name = "Persona";
+            this.Text = "Persona";
+            ((System.ComponentModel.ISupportInitialize)(this.dvgListado)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnListar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dvgListado;
+    }
+}
